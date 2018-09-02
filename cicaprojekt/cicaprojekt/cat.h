70557c4ca86d2acab4c0b00f02f071fb29845a3a
csaba.szigeti <szgtcsaba@gmail.com>
2018-09-02 23:39:46 +0200

    Creates header file for user and cat

---
+++ cicaprojekt/cicaprojekt/cat.h
@@ -0,0 +1,73 @@
+#ifndef CAT_H
+#define CAT_H
+
+#include <string>
+#include <chrono>
+
+class Cat
+{
+
+
+    private:
+        enum class Gender
+        {
+            male,
+            female
+        };
+        enum class HungryState
+        {
+            best,
+            good,
+            bad,
+            worst
+        };
+        enum class SleepyState
+        {
+            best,
+            good,
+            bad,
+            worst
+        };
+        enum class LonelyState
+        {
+            best,
+            good,
+            bad,
+            worst
+        };
+
+        std::string name;
+        Gender gender;
+        
+        int hungry = 100;
+        int sleepy = 100;
+        int lonely = 100;
+
+        double creationTime;
+        double currentTime;
+
+        HungryState hungerState = HungryState::best;
+        SleepyState sleepyState = SleepyState::best;
+        LonelyState lonelyState = LonelyState::best;
+
+        void updateHungry(int value, double currTime);
+        void updateSleepy(int value, double currTime);
+        void updateLonely(int value, double currTime);
+
+public:
+    Cat(std::string name, Gender gender);
+    ~Cat();
+    std::string getName();
+    Gender getGender();
+    HungryState getHungryState();
+    SleepyState getSleepyState();
+    LonelyState getLonelyState();
+    void modifyHungry(int value, double currTime);
+    void modifySleepy(int value, double currTime);
+    void modifyLonely(int value, double currTime);
+
+
+};
+
+#endif // !CAT_H
+
---
+++ cicaprojekt/cicaprojekt/user.h
@@ -0,0 +1,20 @@
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+
+class user
+{
+    public:
+        user();
+        ~user();
+
+    private:
+        std::string username;
+
+
+
+
+};
+
+#endif
